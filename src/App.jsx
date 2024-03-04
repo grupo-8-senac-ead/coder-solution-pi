5946757afc6877306e724f0916680b39cc8b4eeb
Bigodrigo <roesbo1@hotmail.com>
2024-03-04 17:47:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Home/Header/Header'
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <h1>Hello!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Header.css'
+
+function Header() {
+
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
