7e74b81ec63b771345abc4664b5fc93b9eaadfbf
Bigodrigo <roesbo1@hotmail.com>
2024-03-05 15:22:21 -0300

    ajustes layout

--- src/components/Home/Nav/Nav.jsx
+++ src/components/Home/Nav/Nav.jsx
@@ -2,30 +2,30 @@ import './Nav.css';
 
 function Nav() {
   return (
-    <div className='navigation'>
+    <nav className='navigation'>
       <h3>menu</h3>
-      <div className='menu-option'>
-        <img src="./dashboard-logo.png" alt="dashboard-logo" />
-        <p>Dasboard</p>
-      </div>
-      <div className='menu-option'>
-        <img src="./estoque-logo.png" alt="estoque-logo" />
-        <p>Estoque</p>
-      </div>
-      <div className='menu-option'>
-        <img src="./precificacao-logo.png" alt="precificacao-logo" />
-        <p>Precificação</p>
-      </div>
-      <div className='menu-option'>
-        <img src="./pdv-logo.png" alt="pdv-logo" />
-        <p>PDV</p>
-      </div>
-      <div className='menu-option'>
-        <img src="./configuracao-logo.png" alt="configuracao-logo" />
-        <p>Configuração</p>
-      </div>
+        <div className='menu-option'>
+          <img src="./dashboard-logo.png" alt="dashboard-logo" />
+          <p>Dasboard</p>
+        </div>
+        <div className='menu-option'>
+          <img src="./estoque-logo.png" alt="estoque-logo" />
+          <p>Estoque</p>
+        </div>
+        <div className='menu-option'>
+          <img src="./precificacao-logo.png" alt="precificacao-logo" />
+          <p>Precificação</p>
+        </div>
+        <div className='menu-option'>
+          <img src="./pdv-logo.png" alt="pdv-logo" />
+          <p>PDV</p>
+        </div>
+        <div className='menu-option'>
+          <img src="./configuracao-logo.png" alt="configuracao-logo" />
+          <p>Configuração</p>
+        </div>
       <img src="coderso-logo.png" alt="Coderso Logo" className='nav-logo'/>
-    </div>
+    </nav>
   );
 }
 
