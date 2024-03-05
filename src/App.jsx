87b4818e11165aa6b6ef0474de7ead9e3d7736eb
igomarcos <igomarcos@gmail.com>
2024-03-05 17:37:32 -0300

    Correção da tela de dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Login from './components/Auth/Login'
-import HomePage from './components/Home/HomePage'
+//import HomePage from './components/Home/HomePage'
 import './App.css'
+import Dashboard from './components/Dashboard/Dashboard'
+import HomePage from './components/Home/HomePage'
 
 
 function App() {
 
   return (
     <div>
-      <HomePage />
-      {/* <Login /> */}
+      <HomePage /> 
+      {/* <HomePage/> */}
     </div>
   )
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,34 +1,41 @@
+import Header from '../Home/Header/Header'
+import Nav from '../Home/Nav/Nav'
 import './Dashboard.css'
 
 function Dashboard() {
 
     return (
-        <div className="dashboard">
-            <div className="Kpi-pizza">
+        <div>
+            <Header />
+            <div><Nav/></div>
+            <div className="dashboard">
+
+                <div className="Kpi-pizza">
 
 
-                <div className="produtos-estoque">
-                    <div  className='container-azul'>
-                        <div>Estoque</div>
-                        <h1>83</h1>
-                        <p>Produtos</p>
+                    <div className="produtos-estoque">
+                        <div className='container-azul'>
+                            <div>Estoque</div>
+                            <h1>83</h1>
+                            <p>Produtos</p>
+                        </div>
+                        <div className='container-vermelho'>
+                            <div>Produtos sem estoque</div>
+                            <h1>15</h1>
+                            <p>Produtos</p>
+                        </div>
                     </div>
-                    <div className='container-vermelho'>
-                        <div>Produtos sem estoque</div>
-                        <h1>15</h1>
-                        <p>Produtos</p>
+                    <div className="grafico-pizza">
+                        <h2>Produtos Vendidos</h2>
+                        <div>
+                            <img src="/public/grafico-pizza.png" alt="Grafico de pizza" />
+                        </div>
                     </div>
                 </div>
-                <div  className="grafico-pizza">
-                    <h2>Produtos Vendidos</h2>
+                <div className='grafico-linhas'>
                     <div>
-                        <img src="/public/grafico-pizza.png" alt="Grafico de pizza" />
+                        <img src="/public/graficos-linha.png" alt="Grafico de linhas" />
                     </div>
-                </div>
-            </div>
-            <div className='grafico-linhas'>
-                <div>
-                    <img src="/public/graficos-linha.png" alt="Grafico de linhas" />
                 </div>
             </div>
         </div>
